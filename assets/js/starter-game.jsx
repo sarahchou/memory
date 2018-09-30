d72f1fb0d74f9e7b51df96d036ec4a14afd57f17
sarahchou <chou.s@husky.neu.edu>
2018-09-30 17:08:29 -0400

    trying to render cards

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -9,17 +9,16 @@ export default function game_init(root) {
 class MemoryGame extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      cards = this.initGame();
-      this.tries = this.tries.bind(this);
-    };
+    //the state of the game is the cards and the tries
+    this.state = this.initGame();
   }
 
-  //initialize the game
+  //initialize the game - board, number of clicks,
   initGame() {
     return {
-      board = this.initCards();
-      clicks = 0;
+      board: this.initCards();
+      clicks: 0;
+      lastClicked: null;
     }
   }
 
@@ -42,7 +41,7 @@ class MemoryGame extends React.Component {
     allCards[13] = 'G'
     allCards[14] = 'H'
     allCards[15] = 'H'
-    return allCards; //shuffle this later
+    return allCards; //TODO shuffle this later
   }
 
   //reset the game to its initial state
@@ -50,13 +49,81 @@ class MemoryGame extends React.Component {
     this.setState(this.initGame());
   }
 
+  //handles clicks and checks if matched
+  handleClick(cardVal) {
+    this.state.clicks ++;
+
+
+  }
+
+  //helper method for rendering a Card
+  renderCard(i) {
+    let card = this.state.allCards[i];
+    return (<Card value={card.value} matched={card.matched}></Card>)
+
+
+
+  }
+
   //render the game
   render() {
+    return (
+      <div>
+        <h3>Memory Game!</h3>
+        <div className="row">
+          {this.renderCard(0)}
+          {this.renderCard(1)}
+          {this.renderCard(2)}
+          {this.renderCard(3)}
+        </div>
+        <div className="row">
+          {this.renderCard(4)}
+          {this.renderCard(5)}
+          {this.renderCard(6)}
+          {this.renderCard(7)}
+        </div>
+        <div className="row">
+          {this.renderCard(8)}
+          {this.renderCard(9)}
+          {this.renderCard(10)}
+          {this.renderCard(11)}
+        </div>
+        <div className="row">
+          {this.renderCard(12)}
+          {this.renderCard(13)}
+          {this.renderCard(14)}
+          {this.renderCard(15)}
+        </div>
+        <div className="row">
+          <h2 id="clicks">Total Clicks: {this.state.clicks}</h2>
+          <button id=reset onClick{() => this.resetGame()}>Reset</button>
+        </div>
+      </div>
+
+    )
 
   }
 
   //function to make Cards
   function Card(params) {
+    let root = params.root;
+
+
+    if (matched) {
+      return (
+        <div className='matchedCard'>
+          <p>"here"</p> //val of the card
+        </div>
+      )
+
+    }
+    else {
+      return (
+        <div className='card'>
+        <p>?</p></div>
+      )
+
+    }
 
   }
 }
